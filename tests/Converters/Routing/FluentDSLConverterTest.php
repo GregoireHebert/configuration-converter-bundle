7f2ed54a729b229b0e84bdd6d03c59f3332292dc
Alex Rock Ancelet <pierstoval@gmail.com>
2020-04-17 21:36:37 +0200

    Continue the test added to FluentDSLConverterTest

--- tests/Converters/Routing/FluentDSLConverterTest.php
+++ tests/Converters/Routing/FluentDSLConverterTest.php
@@ -12,21 +12,42 @@ use Symfony\Component\Console\Tester\CommandTester;
 
 class FluentDSLConverterTest extends KernelTestCase
 {
-    protected static Application $application;
-    protected static Command $command;
-    protected static CommandTester $commandTester;
+    protected static ?CommandTester $commandTester;
 
     protected function setUp(): void
     {
         self::bootKernel();
-        self::$command = self::$container->get('configuration_converter.command.route_file');
-        self::$commandTester = new CommandTester(self::$command);
-
-        static::assertInstanceOf(RouteFileConverterCommand::class, self::$command);
+        /** @var RouteFileConverterCommand $cmd */
+        $cmd = self::$container->get('configuration_converter.command.route_file');
+        static::assertInstanceOf(RouteFileConverterCommand::class, $cmd);
+        self::$commandTester = new CommandTester($cmd);
     }
 
-    public function testSomething(): void
+    protected function tearDown(): void
     {
-        static::assertInstanceOf(RouteFileConverterCommand::class, self::$command);
+        self::$commandTester = null;
+        parent::tearDown();
+    }
+
+    public function testFluent(): void
+    {
+        $fileName = 'empty_routing_file_to_convert';
+        $outputFile = sprintf("%s/../../Fixtures/App/config/routes/%s.php", __DIR__, $fileName);
+        if (\file_exists($outputFile)) {
+            \unlink($outputFile);
+        }
+
+        $tester = self::$commandTester;
+
+        $tester->execute([
+            'file' => sprintf("config/routes/%s.yaml", $fileName),
+            '--output-format' => 'fluent',
+        ]);
+
+        $display = \explode("\n", $tester->getDisplay(true));
+        static::assertStringStartsWith(' Converting... ', $display[1]);
+        static::assertStringStartsWith(' [OK] Written '.$fileName.'.php ', $display[3]);
+
+        \unlink($outputFile);
     }
 }
---
+++ tests/Fixtures/App/config/routes/empty_routing_file_to_convert.php
@@ -0,0 +1,5 @@
+<?php
+use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
+
+return function (RoutingConfigurator $routes) {
+};
