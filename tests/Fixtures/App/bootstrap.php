d03be4814a3a96cab2624bdd3359ced66628cd4c
Alex Rock Ancelet <pierstoval@gmail.com>
2020-04-17 20:33:14 +0200

    Remove useless stuff & cs fixes for some test files

--- tests/Fixtures/App/bootstrap.php
+++ tests/Fixtures/App/bootstrap.php
@@ -4,13 +4,6 @@ declare(strict_types=1);
 
 namespace ConfigurationConverter\Test\Fixtures\App;
 
-use Doctrine\Common\Annotations\AnnotationRegistry;
-
 date_default_timezone_set('UTC');
 
-$loader = require __DIR__.'/../../../vendor/autoload.php';
-require __DIR__.'/AppKernel.php';
-
-AnnotationRegistry::registerLoader('class_exists');
-
-return $loader;
+require __DIR__.'/../../../vendor/autoload.php';
