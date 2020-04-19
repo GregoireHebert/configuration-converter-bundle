a8731f646f493795f294e97f150d96cc67762d21
Alex Rock Ancelet <pierstoval@gmail.com>
2020-04-19 23:09:40 +0200

    Implement resource loading in YamlFileLoader

--- src/Routing/Loader/YamlFileLoader.php
+++ src/Routing/Loader/YamlFileLoader.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace ConfigurationConverter\Routing\Loader;
 
+use ConfigurationConverter\Routing\Resource\ResourceImport;
 use ConfigurationConverter\Routing\Resource\ResourceImports;
 use Symfony\Component\Yaml\Yaml;
 
@@ -33,6 +34,71 @@ class YamlFileLoader implements LoaderInterface
 
         if (!$parsedYaml) {
             return $imports;
+        }
+
+        foreach ($parsedYaml as $name => $config) {
+            $condition = $config['condition'] ?? null;
+            $defaults = $config['defaults'] ?? [];
+            $host = $config['host'] ?? '';
+            $methods = $config['methods'] ?? [];
+            $options = $config['options'] ?? [];
+            $prefix = $config['prefix'] ?? '';
+            $requirements = $config['requirements'] ?? [];
+            $schemes = $config['schemes'] ?? [];
+            $trailingSlashOnRoot = $config['trailing_slash_on_root'] ?? true;
+            $type = $config['type'] ?? null;
+            $exclude = $config['exclude'] ?? null;
+
+            if (isset($config['controller'])) {
+                $defaults['_controller'] = $config['controller'];
+            }
+            if (isset($config['locale'])) {
+                $defaults['_locale'] = $config['locale'];
+            }
+            if (isset($config['format'])) {
+                $defaults['_format'] = $config['format'];
+            }
+            if (isset($config['utf8'])) {
+                $options['utf8'] = $config['utf8'];
+            }
+            if (!is_array($exclude)) {
+                $exclude = [$exclude];
+            }
+
+            if (isset($config['resource'])) {
+                $imports->addResource(
+                    ResourceImport::fromImport(
+                        $name,
+                        $config['resource'],
+                        $type,
+                        $prefix,
+                        $defaults,
+                        $requirements,
+                        $options,
+                        $host,
+                        $condition,
+                        $schemes,
+                        $methods,
+                        $trailingSlashOnRoot,
+                        $exclude
+                    )
+                );
+            } else {
+                $imports->addResource(
+                    ResourceImport::fromRoute(
+                        $name,
+                        $config['path'],
+                        $defaults,
+                        $requirements,
+                        $options,
+                        $host,
+                        $schemes,
+                        $methods,
+                        $condition
+                    )
+                );
+
+            }
         }
 
         return $imports;
